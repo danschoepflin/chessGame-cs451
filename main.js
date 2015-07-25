d4c8d9563f285c33447eb43603ad6f9a3ebc45fb
sukritchhabra <sukrit.chhabra@gmail.com>
2015-07-25 13:38:46 -0400

    Logic for piece movement deveolped (minor bug)
    * Logic for piece movement developed
    
    * Data-Attributes added. Might convert other classes to data-attributes
    also
    
    * BUG: a piece can be moved twice before compulsion for other color is
    placed
    
    * Uncommented, because a lot is subject to change

--- main.js
+++ main.js
@@ -1,25 +1,50 @@
 $(document).ready(function() {
-    var selectedPiece = undefined;
-    var selectedPosition = undefined;
-    var fullClass;
+
+    var selectedPiece = undefined;          /* [Holds the currently selected piece] @type {[JSON]} */
+    var selectedPosition = undefined;       /* [Holds the position where the selected piece is to be moved] @type {[JSON]} */
+    var fullClass;                          /* [Holds all classes of the selected piece] @type {[String]} */
+    var lastMoveColor;                      /* [What was the color of the last piece that was moved] @type {[JSON]} */
 
     $('td').on('click', function() {
         console.log($(this).find('span').attr('class'));
+        console.log('lastMoveColor: ' + lastMoveColor);
+        console.log($(this).find('span').attr('data-color'));
         if(selectedPiece != undefined) {
+            console.log('selecting SECOND piece');
             selectedPosition = $(this);
-            var block = selectedPosition.find('span');
-            var tempClass = block.attr('class');
 
-            block.removeClass(tempClass);
-            block.addClass(fullClass);
-            selectedPiece.find('span').removeClass(fullClass);
-            selectedPiece = undefined;
-            selectedPosition = undefined;
-        }
-        if($(this).find('span').hasClass('glyphicon') && selectedPiece == undefined) {
-            selectedPiece = $(this);
-            fullClass = $(this).find('span').attr('class');
             console.log(selectedPiece);
+            console.log(selectedPosition);
+
+            var condition = selectedPiece.attr('id') != selectedPosition.attr('id');
+            console.log(condition);
+            if (condition) {
+                var block = selectedPosition.find('span');
+                var tempClass = block.attr('class');
+                console.log('tempClass: ' + tempClass);
+
+                if (tempClass != undefined) {
+                    console.log('executing remove class');
+                    block.removeClass(tempClass);
+                }
+
+                block.addClass(fullClass);
+                selectedPiece.find('span').removeClass(fullClass);
+                selectedPiece = undefined;
+                selectedPosition = undefined;
+            }
+
+        } else {
+            console.log('selecting first piece');
+            if($(this).find('span').hasClass('glyphicon')) {
+                if($(this).find('span').attr('data-color') != lastMoveColor) {
+                    selectedPiece = $(this);
+                    fullClass = $(this).find('span').attr('class');
+                    lastMoveColor = $(this).find('span').attr('data-color');
+                    $(this).find('span').attr('data-color', '');
+                    console.log(selectedPiece);
+                }
+            }
         }
     });
 });
