9479ce72c15de738582a68d9c595befc86e38fd3
sukritchhabra <sukrit.chhabra@gmail.com>
2015-08-14 14:57:36 -0400

    Added unmoved class to pieces. data-piece added. Resolves #15, Fixes #4
    * All pieces have unmoved class which is removed when they are moved.
    * Was only asked for pawns but applied to all as might be useful later,
    specially for Castling.
    * data-piece attribute support added. (Might remove it later since
    glyphicon-piece has already been applied as a class)
    
    * data-color assignment fixed now. Fixes bug where a piece could be moved twice and the limitation for alternating wasn't being applied peoperly

--- main.js
+++ main.js
@@ -10,30 +10,30 @@ $(document).ready(function() {
     var lastMoveColor;                      /* [What was the color of the last piece that was moved] @type {[String]} */
 
     $('td').on('click', function() {
-        console.log($(this).find('span').attr('class'));
-        console.log('lastMoveColor: ' + lastMoveColor);
-        console.log($(this).find('span').attr('data-color'));
         if(selectedPiece != undefined) {
-            console.log('selecting SECOND piece');
             selectedPosition = $(this);
 
-            console.log(selectedPiece);
-            console.log(selectedPosition);
-
             var condition = selectedPiece.attr('id') != selectedPosition.attr('id');
-            console.log(condition);
             if (condition) {
                 var block = selectedPosition.find('span');
                 var tempClass = block.attr('class');
-                console.log('tempClass: ' + tempClass);
 
                 if (tempClass != undefined) {
-                    console.log('executing remove class');
                     block.removeClass(tempClass);
                 }
 
                 block.addClass(fullClass);
+                block.removeClass('unmoved');
+
+                var newColor = selectedPiece.find('span').data('color');
+                var newPiece = selectedPiece.find('span').data('piece');
+                block.attr('data-color', newColor);
+                block.attr('data-piece', newPiece);
+
                 selectedPiece.find('span').removeClass(fullClass);
+                selectedPiece.find('span').attr('data-color', '');
+                selectedPiece.find('span').attr('data-piece', '');
+
                 selectedPiece = undefined;
                 selectedPosition = undefined;
             }
@@ -45,8 +45,6 @@ $(document).ready(function() {
                     selectedPiece = $(this);
                     fullClass = $(this).find('span').attr('class');
                     lastMoveColor = $(this).find('span').attr('data-color');
-                    $(this).find('span').attr('data-color', '');
-                    console.log(selectedPiece);
                 }
             }
         }
