b0983c3e06e67b46aff1dfc1fc0ce9015f051e86
srb95 <Sam@Samanthas-MacBook-Pro.local>
2015-08-17 22:09:10 -0400

    Added move logic except pawn, fixes #5
    added all move validation and logic besides pawn

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 /*global $, jQuery, alert*/
 
 $(document).ready(function() {
-
+    var board;
     var selectedPiece = undefined;          /* [Holds the currently selected piece] @type {[JSON]} */
     var selectedPosition = undefined;       /* [Holds the position where the selected piece is to be moved] @type {[JSON]} */
     var fullClass;                          /* [Holds all classes of the selected piece] @type {[String]} */
@@ -16,30 +16,38 @@ $(document).ready(function() {
             var condition_selectingSamePiece = selectedPiece.attr('id') != selectedPosition.attr('id');
             var condition_selectingSameColor = selectedPiece.find('span').attr('data-color') != $(this).find('span').attr('data-color');
                 //condition_selectingSameColor: Checking if the piece you're moving to is of the same color
+            if (condition_selectingSamePiece && condition_selectingSameColor) 
+            {
+                board = getCurrentBoard();
+                var pieceType = selectedPiece.find("span").attr('data-piece');
+                var idNumberPiece = selectedPiece.attr('id');
+                var idNumberSpot = selectedPosition.attr('id');
+                if(isValidMove(board, pieceType, idNumberPiece, idNumberSpot))
+                {
+                    var block = selectedPosition.find('span');
+                    var tempClass = block.attr('class');
 
-            if (condition_selectingSamePiece && condition_selectingSameColor) {
-                var block = selectedPosition.find('span');
-                var tempClass = block.attr('class');
+                    if (tempClass != undefined) {
+                        block.removeClass(tempClass);
+                    }
 
-                if (tempClass != undefined) {
-                    block.removeClass(tempClass);
+                    block.addClass(fullClass);
+                    block.removeClass('unmoved');
+
+                    var newColor = selectedPiece.find('span').data('color');
+                    var newPiece = selectedPiece.find('span').data('piece');
+                    block.attr('data-color', newColor);
+                    block.attr('data-piece', newPiece);
+
+                    selectedPiece.find('span').removeClass(fullClass);
+                    selectedPiece.removeClass('selected');
+                    selectedPiece.find('span').attr('data-color', '');
+                    selectedPiece.find('span').attr('data-piece', '');
+
+                    selectedPiece = undefined;
+                    selectedPosition = undefined;
                 }
 
-                block.addClass(fullClass);
-                block.removeClass('unmoved');
-
-                var newColor = selectedPiece.find('span').data('color');
-                var newPiece = selectedPiece.find('span').data('piece');
-                block.attr('data-color', newColor);
-                block.attr('data-piece', newPiece);
-
-                selectedPiece.find('span').removeClass(fullClass);
-                selectedPiece.removeClass('selected');
-                selectedPiece.find('span').attr('data-color', '');
-                selectedPiece.find('span').attr('data-piece', '');
-
-                selectedPiece = undefined;
-                selectedPosition = undefined;
             }
 
         } else {
@@ -58,56 +66,46 @@ $(document).ready(function() {
 
 function isValidMove(board, pieceType, idNumberPiece, idNumberSpot)
 {
-    var spotIsEmpty = isSpotEmpty(board, idNumberSpot);
     var idNumPiece = parseInt(idNumberPiece) + 1;
-    var rowPiece = idNumPiece/8;
+    var rowPiece = Math.ceil(idNumPiece/8);
     var colPiece = idNumPiece % 8;
 
-    var idNumSpot = parseInt(idNumberSpot);
-    var rowSpot = idNumSpot/8;
+    var idNumSpot = parseInt(idNumberSpot) + 1;
+    var rowSpot = Math.ceil(idNumSpot/8);
     var colSpot = idNumSpot % 8;
 
-    if((pieceType == "pawn") && (spotIsEmpty) && ((idNumPiece - 8 == idNumSpot) || (idNumPiece - 16 == idNumSpot)))
+    if((pieceType == "pawn") && ((idNumPiece - 8 == idNumSpot) || (idNumPiece - 16 == idNumSpot)))
     {
         return true;
     }
 
-    if((pieceType == "bishop") && (spotIsEmpty) && (isDiagonal(rowPiece, colPiece, rowSpot, colSpot)))
+    if((pieceType == "bishop") && (isDiagonal(rowPiece, colPiece, rowSpot, colSpot)))
     {
         return true;
     }
 
-    if((pieceType == "queen") && (spotIsEmpty) && (isDiagonal(rowPiece, colPiece, rowSpot, colSpot) || isVertical(rowPiece, colPiece, rowSpot, colSpot) || isHorizontal(rowPiece, colPiece, rowSpot, colSpot)))
+    if((pieceType == "queen") && (isDiagonal(rowPiece, colPiece, rowSpot, colSpot) || isVertical(rowPiece, colPiece, rowSpot, colSpot) || isHorizontal(rowPiece, colPiece, rowSpot, colSpot)))
     {
         return true;
     }
 
-    if((pieceType == "king") && (spotIsEmpty) && ((Math.abs(rowPiece - rowSpot) <= 1 && Math.abs(colPiece - colSpot) <= 1)))
+    if((pieceType == "king") && ((Math.abs(rowPiece - rowSpot) <= 1 && Math.abs(colPiece - colSpot) <= 1)))
     {
         return true;
     }
 
-    if((pieceType == "knight") && (spotIsEmpty) && ((Math.abs(xfrom - xto) == 1 && Math.abs(yfrom - yto) == 2) || (Math.abs(yfrom - yto) == 1 && Math.abs(xfrom - xto) == 2)))
+    if((pieceType == "knight") && ((Math.abs(rowPiece - rowSpot) == 1 && Math.abs(colPiece - colSpot) == 2) || (Math.abs(colPiece - colSpot) == 1 && Math.abs(rowPiece - rowSpot) == 2)))
     {
         return true;
     }
 
-    if((pieceType == "tower") && (spotIsEmpty) && (isVertical(rowPiece, colPiece, rowSpot, colSpot) || isHorizontal(rowPiece, colPiece, rowSpot, colSpot)))
+    if((pieceType == "tower") && (isVertical(rowPiece, colPiece, rowSpot, colSpot) || isHorizontal(rowPiece, colPiece, rowSpot, colSpot)))
     {
         return true;
     }
 
 
     return false;
-}
-
-function isSpotEmpty(board, idNumberSpot)
-{
-    int idNumSpot = parseInt(idNumberSpot) + 1;
-    int rowSpot = idNumSpot/8;
-    int colSpot = idNumSpot % 8;
-
-    return board[rowSpot][colSpot] === "";
 }
 
 function isDiagonal(rowPiece, colPiece, rowSpot, colSpot)
@@ -123,4 +121,46 @@ function isVertical(rowPiece, colPiece, rowSpot, colSpot)
 function isHorizontal(rowPiece, colPiece, rowSpot, colSpot)
 {
     return (colPiece == colSpot && rowPiece != rowSpot);
+}
+
+function getCurrentBoard()
+{
+    var board = [];
+    var row = [];
+    $("td").each(function() {
+        var isNotBlank = $(this).find("span").attr("data-color");
+
+        if (typeof isNotBlank !== typeof undefined && isNotBlank !== false)
+        {
+            if(((row.length + 1) % 9) == 0)
+            {
+                board.push(row);
+                row = [];
+                var color = $(this).find("span").attr('data-color');
+                var pieceName = $(this).find("span").attr('data-piece');
+                row.push(color + " " + pieceName);
+            }
+            else
+            {
+                var color = $(this).find("span").attr('data-color');
+                var pieceName = $(this).find("span").attr('data-piece');
+                row.push(color + " " + pieceName);
+            }
+        }
+        else
+        {
+            if(((row.length + 1) % 9) == 0)
+            {
+                board.push(row);
+                row = [];
+                row.push("");
+            }
+            else
+            {
+                row.push("");
+            }
+        }
+    });
+    board.push(row);
+    return board;
 }
