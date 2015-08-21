0dd8ddcd70d5bc16902ad674d3709dbc16852249
danschoepflin <daniel.r.schoepflin@gmail.com>
2015-08-21 17:59:35 -0400

    New base project with functional networking.

---
+++ Chesstacular1/Chesstacular1/src/main/java/com/mycompany/mavenproject1/MyChessgame.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import javax.websocket.EncodeException;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ *
+ * @author allisonfrauenpreis
+ */
+@ServerEndpoint (value="/chessgameendpoint", encoders = {ChessboardEncoder.class}, decoders = {ChessboardDecoder.class})
+
+public class MyChessgame {
+    
+    // list of players connected to server
+    private static Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
+
+    @OnMessage
+    public void broadcastFigure(Chessboard chessboard, Session session) throws IOException, EncodeException {
+        System.out.println("broadcastFigure: " + chessboard);
+        for (Session peer : peers) {
+            if (!peer.equals(session)) {
+                peer.getBasicRemote().sendObject(chessboard);
+            }
+        }
+    }
+    
+    // code to be injected into interface
+    @OnOpen
+    public void onOpen (Session peer)
+    {
+        peers.add(peer);
+    }
+    
+    @OnClose
+    public void onClose (Session peer)
+    {
+        peers.remove(peer);
+    }
+    
+    
+}
